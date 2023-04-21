33f09d4c402237c5f2e8b08a70198e4871328d22
Jean Suh <jeansuh@Jeans-MBP-2.lan>
2023-04-21 15:56:27 -0400

    fixed basename

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react">
+      <Router basename="/bank-of-react/">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
