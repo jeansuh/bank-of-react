93fa94ab111890268ff73c7382103273a9cdae76
Jean Suh <jeansuh@Jeans-MBP-2.lan>
2023-04-21 15:11:16 -0400

    added Debits.js constructor

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -6,31 +6,24 @@ Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
 
-const Debits = (props) => {
-  // Create the list of Debit items
-  let debitsView = () => {
-    const { debits } = props;
-    return debits.map((debit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debit.date.slice(0,10);
-      return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-    });
+class Debits extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      description: '', //Initializes states that will be used in debitList
+      amount : '',
+      date: '',
+      debitList:this.props.debitList,
+      accountBalance:this.props.accountBalance //initializes using props
+    }
   }
-  // Render the list of Debit items and a form to input new Debit item
-  return (
-    <div>
-      <h1>Debits</h1>
 
-      {debitsView()}
+  render() {
+    return(
+      <div>Debits</div>
+    )
+  }
 
-      <form onSubmit={props.addDebit}>
-        <input type="text" name="description" />
-        <input type="number" name="amount" />
-        <button type="submit">Add Debit</button>
-      </form>
-      <br/>
-      <Link to="/">Return to Home</Link>
-    </div>
-  );
 }
 
 export default Debits;
