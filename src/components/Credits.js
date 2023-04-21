28e3a00de0b1b6cc3ee86cb287f6cf1d62f5d9f2
Jean Suh <jeansuh@Jeans-MBP-2.lan>
2023-04-21 15:06:58 -0400

    added contructor for credit.js

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,15 +5,28 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
 
-const Credits = (props) => {
-  return (
-    <div>
-      <h1>Credits</h1>
-      <br/>
-      <Link to="/">Return to Home</Link>
-    </div>
-  );
+class Credits extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      description: '', //Initializes variables that will be used in creditList
+      amount: '',
+      date:'',
+      creditList:this.props.creditList,
+      accountBalance:this.props.accountBalance //Initializes by using props
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Credits</h1>
+      </div>
+    )
+  }
+
 }
 
 export default Credits;
