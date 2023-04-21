45543cb1dc74fb8e6fac7c46f470f5a495e328aa
Jean Suh <jeansuh@Jeans-MBP-2.lan>
2023-04-21 15:08:26 -0400

    added handleChange for form submission

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -19,11 +19,27 @@ class Credits extends Component {
     }
   }
 
+  handleChange = (e, type) => {
+    this.setState({...this.state, [type]: e.target.value}); 
+    console.log(e.target.value);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Credits</h1>
-      </div>
+        <div>
+          <h1>Credits</h1>
+          <ul>
+            {creditList.map((credit) =>
+              <li style={{listStyle:'none'}}>{`${credit.description} : ${credit.amount} ${credit.date.slice(0,10)}`} </li>)}
+          </ul>
+          <div>Account balance : {`${accountBalance}`}</div>
+          <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.description} onChange={(e) => {this.handleChange(e, "description")}} ref="description" />
+              <input type="text" value={this.state.amount} onChange={(e) => {this.handleChange(e, "amount")}} ref="amount" />
+            <button>Add Credit</button>
+          </form>
+          <Link to='/'>Return to Home</Link>
+        </div>
     )
   }
 
